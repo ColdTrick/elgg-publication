3d4f35660ad95f5baf826587a82dd41e3f85eb62
Jerome Bakker <jeabakker@coldtrick.com>
2013-06-27 14:47:33 +0200

    changed: moved plugin hooks to own library file

--- start.php
+++ start.php
@@ -8,6 +8,7 @@
 	*/
 
 	require_once(dirname(__FILE__) . "/lib/functions.php");
+	require_once(dirname(__FILE__) . "/lib/hooks.php");
 	require_once(dirname(__FILE__) . "/lib/page_handlers.php");
 
 	function publication_init() {
@@ -62,52 +63,6 @@
 		elgg_register_plugin_hook_handler("register", "menu:filter", "publication_register_menu_filter");
 	}
 		
-	function publication_register_menu_filter($hook, $type, $return_value, $params) {
-		if(elgg_in_context("publications")) {
-			foreach($return_value as $index => $menu_item) {
-				if($menu_item->getName() == "friend"){
-					unset($return_value[$index]);
-				}
-			}
-			
-			return $return_value;
-		}
-	}
-	
-	function publication_register_menu_owner_block($hook, $type, $return_value, $params) {
-		if (elgg_instanceof($params['entity'], 'user')) {
-			$return_value[] = ElggMenuItem::factory(array(
-				"name" => "publications",
-				"text" => elgg_echo("publications"),
-				"href" => "publications/owner/" . $params["entity"]->username,
-					
-			));
-		} elseif (elgg_instanceof($params['entity'], 'group')) {
-			if($params['entity']->publications_enable != "no") {
-				$url = '/publications/group/' . $params['entity']->getGUID() . "/all";
-				$item = new ElggMenuItem('publications', elgg_echo('publication:group'), $url);
-				$return_value[] = $item;
-			}
-		}
-			
-		return $return_value;
-	}
-	
-	function publication_register_menu_page($hook, $type, $return_value, $params) {
-		if (elgg_is_logged_in()) {
-			// 	import
-			$return_value[] = ElggMenuItem::factory(array(
-					"name" => "bibtex_import",
-					"text" => elgg_echo("publication:import"),
-					"href" => "publications/import",
-					"section" => "bibtex",
-					"context" => "publications"
-			));
-		}
-		
-		return $return_value;
-	}
-	
 	//extend the create user function to include additional information
 	//for invited authors
 	function publication_create_user($event, $object_type, $object){
@@ -148,70 +103,6 @@
 		}
 	}
 
-	/* adds write rights to publication authors */
-	function publication_write_permission_check($hook, $entity_type, $returnvalue, $params){
-		if ($params['entity']->getSubtype() == 'publication') {
-			if ($user_guid = $params['user']->guid){
-				if (check_entity_relationship($params['entity']->guid, 'author', $user_guid)) {
-					return true;
-				}
-			}
-		}
-	}
-
-	/* replaced register plugin when we have a an invited author */
-// 	function publication_custom_register($hook,$entity_type,$ret,$params){
-// 		global $CONFIG;
-// 		// Get variables
-// 		$publication = get_input('publication');
-// 		$author = get_input('author');
-// 		if(!($author && $publication)) return;
-// 		$username = get_input('username');
-// 		$password = get_input('password');
-// 		$password2 = get_input('password2');
-// 		$email = get_input('email');
-// 		$name = get_input('name');
-// 		$friend_guid = (int) get_input('friend_guid',0);
-// 		$invitecode = get_input('invitecode');
-// 		$admin = get_input('admin');
-// 		if (is_array($admin)) $admin = $admin[0];
-
-
-// 		if (!$CONFIG->disable_registration){
-// 			// For now, just try and register the user
-// 			try {
-// 				if (((trim($password)!="") && (strcmp($password, $password2)==0)) && ($guid = register_user($username, $password, $name, $email, false, $friend_guid, $invitecode))) {
-// 					$new_user = get_entity($guid);
-// 					if (($guid) && ($admin)){
-// 						admin_gatekeeper();
-// 						$new_user->admin = 'yes';
-// 					}
-// 					// Send user validation request on register only
-// 					global $registering_admin;
-// 					if (!$registering_admin)
-// 						request_user_validation($guid);
-
-// 					if (!$new_user->admin)
-// 						$new_user->disable('new_user', false);
-// 					system_message(sprintf(elgg_echo("registerok"),$CONFIG->sitename));
-// 					forward(); // Forward on success, assume everything else is an error...
-// 				} else {
-// 					register_error(elgg_echo("registerbad"));
-// 				}
-// 			} catch (RegistrationException $r) {
-// 				register_error($r->getMessage());
-// 			}
-// 		}
-// 		else
-// 			register_error(elgg_echo('registerdisabled'));
-
-// 		$qs = explode('?',$_SERVER['HTTP_REFERER']);
-// 		$qs = $qs[0];
-// 		$qs .= "?u=" . urlencode($username) . "&e=" . urlencode($email) . "&n=" . urlencode($name) . "&friend_guid=" . $friend_guid . "&invidecode=". $invitecode ."&author=". urlencode($author) . "&publication=".$publication;
-// 		forward($qs);
-// 	}
-
-	
 	function publication_pagesetup() {
 		// Set up menu for logged in users
 		if (elgg_is_logged_in()) {
@@ -228,25 +119,6 @@
 					"href" => "mod/publications/everyone.php"
 			));
 		}
-	}
-
-	function publication_notify_message($hook, $entity_type, $returnvalue, $params){
-		$entity = $params['entity'];
-		$to_entity = $params['to_entity'];
-		$method = $params['method'];
-		if (($entity instanceof ElggEntity) && ($entity->getSubtype() == 'publication')){
-			$descr = $entity->description;
-			$title = $entity->title;
-			if ($method == 'sms') {
-				$owner = $entity->getOwnerEntity();
-				return $owner->username . ' via publication: ' . $title;
-			}
-			if ($method == 'email') {
-				$owner = $entity->getOwnerEntity();
-				return $owner->username . ' via publication: ' . $title . "\n\n" . $descr . "\n\n" . $entity->getURL();
-			}
-		}
-		return null;
 	}
 
 	function publication_url($entity) {
