b31be6b690255ed13369c0b0651deb18680c0493
Jerome Bakker <jeabakker@coldtrick.com>
2013-06-27 15:08:01 +0200

    Revert "changed: moved event handler functions to own library file changed: layout of start.php to be better inline with elgg standards"
    This reverts commit 6121e2f290dd765dbe64289bf8aeb00f6134a0e2.

--- start.php
+++ start.php
@@ -8,100 +8,136 @@
 	*/
 
 	require_once(dirname(__FILE__) . "/lib/functions.php");
-	require_once(dirname(__FILE__) . "/lib/events.php");
 	require_once(dirname(__FILE__) . "/lib/hooks.php");
 	require_once(dirname(__FILE__) . "/lib/page_handlers.php");
 
-	elgg_register_event_handler("init", "system", "publication_init");
-	
 	function publication_init() {
 	
 		// Load system configuration
 		
 		if (elgg_get_plugin_setting("enable_bibtex", "publications") == "yes") {
-			// register vendor classes for autoload
-			elgg_register_classes(dirname(__FILE__) . "/vendors/bibtex/");
+			require_once(dirname(__FILE__) . "/vendors/bibtex/PARSEENTRIES.php");
+			require_once(dirname(__FILE__) . "/vendors/bibtex/PARSECREATORS.php");
 			
-			// extend the page menu
 			elgg_register_plugin_hook_handler("register", "menu:page", "publication_register_menu_page");
 			
-			// register some additional actions
-			elgg_register_action("publications/import", dirname(__FILE__) . "/actions/import.php");
-			elgg_register_action("publications/export", dirname(__FILE__) . "/actions/export.php");
+			elgg_register_action("publications/import", elgg_get_plugins_path() . "publications/actions/import.php");
+			elgg_register_action("publications/export", elgg_get_plugins_path() . "publications/actions/export.php");
 			
 		}
 		
 		// extend javascript
 		elgg_extend_view("js/elgg", "js/publications/site");
-		elgg_extend_view("css/elgg", "css/publications/site");
 		
+		// Extend hover-over menu
+		elgg_extend_view('profile/menu/links','publication/menu');
+		elgg_extend_view('account/forms/register','publication/register');
+				
 		// Register a page handler, so we can have nice URLs
-		elgg_register_page_handler("publications", "publication_page_handler");
+		elgg_register_page_handler('publications','publication_page_handler');
 		
 		// Register a URL handler for publication posts
-		elgg_register_entity_url_handler("object", "publication", "publication_url");
+		elgg_register_entity_url_handler('object','publication', 'publication_url');
 		
 		// Add a new widget
-		elgg_register_widget_type("publications", elgg_echo("publications:widget"), elgg_echo("publications:widget:description"));
+		elgg_register_widget_type('publications',elgg_echo("publications:widget"),elgg_echo("publications:widget:description"));
 		
 		// Register granular notification for this type
-		register_notification_object("object", "publication", elgg_echo("publication:newpost"));
+		if (is_callable('register_notification_object')) {
+			register_notification_object('object', 'publication', elgg_echo('publication:newpost'));
+		}
 		
 		// Listen to notification events and supply a more useful message
-		elgg_register_plugin_hook_handler("notify:entity:message", "object", "publication_notify_message");
+		elgg_register_plugin_hook_handler('notify:entity:message', 'object', 'publication_notify_message');
 		
-		// Register entity type for search
-		elgg_register_entity_type("object", "publication");
+		// Listen for new pingbacks
+		elgg_register_event_handler('create', 'object', 'publication_incoming_ping');
+		
+		// Register entity type
+		elgg_register_entity_type('object','publication');
 			
-		// add group option
-		add_group_tool_option("publication", elgg_echo("publication:enablepublication"), true);
+		add_group_tool_option('publication', elgg_echo('publication:enablepublication'), true);
 		
-		// register plugin hooks
+		// menu setup hooks
 		elgg_register_plugin_hook_handler("register", "menu:owner_block", "publication_register_menu_owner_block");
 		elgg_register_plugin_hook_handler("register", "menu:filter", "publication_register_menu_filter");
-// 		elgg_register_plugin_hook_handler("action","register","publication_custom_register");
-		elgg_register_plugin_hook_handler("permissions_check", "object", "publication_write_permission_check");
-		
-		// register event handlers
-		elgg_register_event_handler("login", "user", "publication_login_check");
-		
-		// Make sure the publication initialisation function is called on initialisation
-		elgg_register_event_handler("pagesetup", "system", "publication_pagesetup");
-		elgg_register_event_handler("create", "user", "publication_create_user");
-		
-		// Register actions
-		elgg_register_action("publications/add", dirname(__FILE__) . "/actions/add.php");
-		elgg_register_action("publications/edit", dirname(__FILE__) . "/actions/edit.php");
-		elgg_register_action("publications/delete", dirname(__FILE__) . "/actions/delete.php");
-// 		elgg_register_action("publications/invite", dirname(__FILE__) . "/actions/invite.php");
-		
 	}
 		
+	//extend the create user function to include additional information
+	//for invited authors
+	function publication_create_user($event, $object_type, $object){
+		foreach ($_POST as $key=>$value) {
+			if ($key == 'author') {
+				$author = $value;
+			}
+			if ($key == 'publication') {
+				$publication = $value;
+			}
+		}
+		
+		if ($author && $publication) {
+			create_metadata($object->guid, 'exauthor_name', $author, "", $object->guid, ACCESS_PUBLIC);
+			create_metadata($object->guid, 'firstpublication', $publication, "", $object->guid, ACCESS_PUBLIC);
+		}
+	}
+
+	/* Updates author's list when an invited author registers */
+	function publication_login_check($event, $object_type, $object){
+		$user = elgg_get_logged_in_user_entity();
+		if($user->firstpublication && $user->exauthor_name){
+			$exauthor_name = $user->exauthor_name;
+			$pub = get_entity($user->firstpublication);
+			add_entity_relationship($user->firstpublication, 'author', $user->guid);
+			remove_metadata($user->guid, 'firstpublication');
+			remove_metadata($user->guid, 'exauthor_name');
+			$authors = $pub->authors;
+			$authors = explode(',',$authors);
+			
+			foreach ($authors as $key=>$value) {
+				if ($value == $exauthor_name) {
+					$authors[$key] = $user->guid;
+				}
+			}
+			$authors = implode(',',$authors);
+			$pub->authors = $authors;
+		}
+	}
+
 	function publication_pagesetup() {
 		// Set up menu for logged in users
-		if ($user = elgg_get_logged_in_user_entity()) {
+		if (elgg_is_logged_in()) {
 			elgg_register_menu_item("site", array(
-				"name" => "publications",
-				"text" => elgg_echo("publications"),
-				"href" => "publications/owner/" . $user->username
+					"name" => "publications",
+					"text" => elgg_echo("publications"),
+					"href" => "publications/" . elgg_get_logged_in_user_entity()->username
 			));
 		} else {
 			// And for logged out users
 			elgg_register_menu_item("site", array(
-				"name" => "publications",
-				"text" => elgg_echo("publications"),
-				"href" => "publications/all"
+					"name" => "publications",
+					"text" => elgg_echo("publications"),
+					"href" => "mod/publications/everyone.php"
 			));
 		}
 	}
 
-	/**
-	 * Make sure we create a correct URL for a publication
-	 *
-	 * @param ElggObject $entity
-	 * @return string
-	 */
-	function publication_url(ElggObject $entity) {
+	function publication_url($entity) {
 		return elgg_get_site_url() . "publications/view/" . $entity->getGUID() . "/" . elgg_get_friendly_title($entity->title);
 	}
+
+	// write permission plugin hooks
+	elgg_register_plugin_hook_handler('permissions_check', 'object', 'publication_write_permission_check');
+	elgg_register_event_handler('login','user','publication_login_check');
 	
+	// Make sure the publication initialisation function is called on initialisation
+	elgg_register_event_handler('init','system','publication_init');
+	elgg_register_event_handler('pagesetup','system','publication_pagesetup');
+	elgg_register_event_handler('create','user','publication_create_user');
+	
+// 	elgg_register_plugin_hook_handler('action','register','publication_custom_register');
+
+	// Register actions
+	elgg_register_action("publications/add", elgg_get_plugins_path() . "publications/actions/add.php");
+	elgg_register_action("publications/edit", elgg_get_plugins_path() . "publications/actions/edit.php");
+	elgg_register_action("publications/delete", elgg_get_plugins_path() . "publications/actions/delete.php");
+// 	elgg_register_action("publications/invite", elgg_get_plugins_path() . "publications/actions/invite.php");
