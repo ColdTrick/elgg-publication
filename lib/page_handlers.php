84f532cbf5ce8f8d2a610ed403a5f6439f7b70e1
Jerome Bakker <jeabakker@coldtrick.com>
2013-06-27 14:27:18 +0200

    forgot to change the include paths

--- lib/page_handlers.php
+++ lib/page_handlers.php
@@ -7,35 +7,35 @@
 	
 		switch($page[0]){
 			case "all":
-				include(dirname(__FILE__) . "/pages/all.php");
+				include(dirname(dirname(__FILE__)) . "/pages/all.php");
 				break;
 			case "group":
 				if(isset($page[1])){
 					set_input("guid", $page[1]);
 				}
-				include(dirname(__FILE__) . "/pages/group.php");
+				include(dirname(dirname(__FILE__)) . "/pages/group.php");
 				break;
 			case "owner":
-				include(dirname(__FILE__) . "/pages/owner.php");
+				include(dirname(dirname(__FILE__)) . "/pages/owner.php");
 				break;
 			case "add":
-				include(dirname(__FILE__) . "/pages/edit.php");
+				include(dirname(dirname(__FILE__)) . "/pages/edit.php");
 				break;
 			case "edit":
 				if(isset($page[1])){
 					set_input("guid", $page[1]);
 				}
-				include(dirname(__FILE__) . "/pages/edit.php");
+				include(dirname(dirname(__FILE__)) . "/pages/edit.php");
 				break;
 			case "view":
 				if(isset($page[1])){
 					set_input("guid", $page[1]);
 				}
-				include(dirname(__FILE__) . "/pages/view.php");
+				include(dirname(dirname(__FILE__)) . "/pages/view.php");
 				break;
 			case "import":
 				if (elgg_get_plugin_setting("enable_bibtex", "publications") == "yes") {
-					include(dirname(__FILE__) . "/pages/import.php");
+					include(dirname(dirname(__FILE__)) . "/pages/import.php");
 					break;
 				}
 			default:
