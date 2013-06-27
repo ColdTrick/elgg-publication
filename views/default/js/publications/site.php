2270f087f09e3fb9eeb898724b434f60b21ec66c
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-06-27 16:09:48 +0200

    removed extra slash

--- views/default/js/publications/site.php
+++ views/default/js/publications/site.php
@@ -5,7 +5,7 @@ elgg.provide("elgg.publications");
 
 elgg.publications.draw_custom_fields = function(type, guid){
 	$.ajax({
-		url: elgg.get_site_url() + '/publications/custom_fields',
+		url: elgg.get_site_url() + 'publications/custom_fields',
 		data:'type='+type+'&guid='+guid,
 		success:function(data){
 			$('#pub_custom_fields').html(data);
