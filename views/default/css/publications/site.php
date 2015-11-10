43926beb8cac1d0b6d56c134b2b22272d2af4c12
Jerome Bakker <jeabakker@coldtrick.com>
2015-11-10 15:03:25 +0100

    fixed: book editors not dragable and unable to delete

--- views/default/css/publications/site.php
+++ views/default/css/publications/site.php
@@ -5,11 +5,17 @@
 	font-weight: bold;
 }
 
-.publications-authors_autocomplete_results_result {
+.elgg-form-publications-edit .ui-helper-hidden-accessible {
+	display: none;
+}
+
+.publications-authors_autocomplete_results_result,
+.publications-book-editors_autocomplete_results_result {
 	cursor: move;
 }
 
-.publications-authors_autocomplete_results_result .elgg-icon {
+.publications-authors_autocomplete_results_result .elgg-icon,
+.publications-book-editors_autocomplete_results_result .elgg-icon {
 	cursor: pointer;
 }
 
